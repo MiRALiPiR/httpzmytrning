c5bd35cc468af7b729fbe29efc8b42d6ae79457a
MiRALiPiR <miralimhmdi2007@gmail.com>
2024-12-26 07:11:42 +0000

    08

--- app.py
+++ app.py
@@ -1,47 +1,18 @@
 import os
-import logging
-from telebot import TeleBot
-import requests
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+from dotenv import load_dotenv
+from telethon import Button, TelegramClient, events
+from telethon.tl.custom.message import Message
 
-bot_token = '7656738137:AAGahLO-G_TmYGlD6mx5ofgu1_5GZyKkrRo' # توکن ربات از فایل .env
-INSTAGRAM_API_BASE_URL = "http://127.0.0.1:5000/api/instagram/download"  # آدرس API فلسک شما
-API_KEY = "کلید دسترسی"
+load_dotenv()
 
-bot = TeleBot(bot_token)
+API_ID = os.getenv('api_id')
+API_HASH = os.getenv('api_hash')
+TOKEN = os.getenv('token')
+client = TelegramClient('bot_session', api_id=API_ID, api_hash=API_HASH ).start(bot_token=TOKEN)
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, "سلام! لطفاً لینک پست اینستاگرام را ارسال کنید.")
+@client.on(events.NewMessage(pattern=r'/start'))
+async def start(event: Message):
+    await event.respond('Hi Babe')
 
-@bot.message_handler(func=lambda message: True)
-def handle_message(message):
-    url = message.text.strip()
-    if "instagram.com" not in url:
-        bot.reply_to(message, "❌ لطفاً یک لینک معتبر اینستاگرام ارسال کنید.")
-        return
-
-    try:
-        bot.reply_to(message, "⏳ در حال پردازش لینک، لطفاً منتظر بمانید...")
-        # درخواست به وب‌سرویس فلسک
-        response = requests.get(INSTAGRAM_API_BASE_URL, params={"url": url, "key": API_KEY})
-        response_data = response.json()
-
-        if response.status_code == 200:
-            download_url = response_data.get("download_url")
-            if download_url:
-                # دانلود فایل و ارسال به کاربر
-                file_response = requests.get(download_url)
-                bot.send_video(message.chat.id, file_response.content)
-            else:
-                bot.reply_to(message, "❌ لینک دانلود یافت نشد.")
-        else:
-            bot.reply_to(message, response_data.get("error", "❌ مشکلی پیش آمد."))
-    except Exception as e:
-        logger.error(f"Error handling Instagram link: {e}")
-        bot.reply_to(message, "❌ خطایی رخ داد. لطفاً دوباره تلاش کنید.")
-
-if __name__ == "__main__":
-    bot.polling(none_stop=True)
+client.run_until_disconnected()
